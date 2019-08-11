99c77aa9d637e12b7415e5231a97a3baa698bf97
Federico Paparoni <federico.paparoni@gmail.com>
2019-08-11 17:08:25 +0200

    rename

--- src/main/java/com/javastaff/spring/k8config/TestConfigController.java
+++ src/main/java/com/javastaff/spring/k8config/TestConfigController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClientController {
+public class TestConfigController {
 	
 	@Autowired
 	private Config config;
